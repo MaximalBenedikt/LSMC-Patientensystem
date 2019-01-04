6932dab3affa4b36b0d93681105ee07f189ff32e
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2019-01-04 04:21:03 +0100

    FIX: Probleme mit Umlauten

--- data.php
+++ data.php
@@ -2,13 +2,13 @@
     include('config.php');
     $pdo = new PDO( $config['dbhost'] , $config['dbuser'] , $config['dbpass'] );
     function utf8ize($mixed) {
-        if (is_array($mixed)) {
+        /*if (is_array($mixed)) {
             foreach ($mixed as $key => $value) {
                 $mixed[$key] = utf8ize($value);
             }
         } else if (is_string ($mixed)) {
             return utf8_encode($mixed);
-        }
+        }*/
         return $mixed;
     }
 
