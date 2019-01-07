a7662fc206ff641c9e76985da6677b64cf837944
MaximalBenedikt <45009547+MaximalBenedikt@users.noreply.github.com>
2019-01-07 01:20:02 +0100

    Testchange

--- data.php
+++ data.php
@@ -51,6 +51,7 @@
         $statement->execute();
         $return = $statement->fetch();
         echo json_encode(encodeUTFarray($return));
+        //Testchange
     }
     //Speichern
     if ($_POST['action'] == 'savepatient') {
